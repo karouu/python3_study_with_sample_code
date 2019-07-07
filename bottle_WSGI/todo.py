bf6e5b86f2badc1a796e38f2ab2b54a3809ed7cf
karouu <chenda721@gmail.com>
2019-07-07 11:05:28 +0800

    add edit routing and template

--- bottle_WSGI/todo.py
+++ bottle_WSGI/todo.py
@@ -37,5 +37,28 @@ def new_item():
     else:
         return template('new_task.tpl')
 
+@route('/edit/<no:int>', method='GET')
+def edit_item(no):
+    if request.GET.save:
+        edit = request.GET.task.strip()
+        status = request.GET.status.strip()
+        if status == 'open':
+            status = 1
+        else:
+            status = 0
+
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("UPDATE todo SET task = ?, status = ? WHERE id LIKE ?", (edit, status, no))
+        conn.commit()
+        return '<p>The item number %s was successfully updated</p>' % no
+    else:
+        conn = sqlite3.connect('todo.db')
+        c = conn.cursor()
+        c.execute("SELECT task FROM todo WHERE id LIKE ?", (str(no),))
+        cur_data = c.fetchone()
+
+        return template('edit_task', old=cur_data, no=no)
+
 debug(True)
 run(host='0.0.0.0', port=8080, reloader=True)
